ef9a4fa0506fa201ad81fb0696717d9d703de304
Zhiquan.Wang <mySabreHawk@outlook.com>
2017-12-13 19:44:35 +0800

    Add Solution : 5 2017/12/13 - 19:44

--- Basic-Data-Types/5-Nested-Lists.py
+++ Basic-Data-Types/5-Nested-Lists.py
@@ -7,10 +7,14 @@ if __name__ == '__main__':
         score = float(input())
         markSheet.append([name,score])
     markSheet.sort(key = operator.itemgetter(1))
-    sec_score = markSheet[1][1]
+    max_score = markSheet[0][1]
+    sec_score = -1;
     for i in range(len(markSheet)):
-        if markSheet[i][1] == sec_score:
+        if markSheet[i][1] != max_score and sec_score == -1:
+            sec_score = markSheet[i][1]
+        if sec_score != -1 and markSheet[i][1] == sec_score:
             nameSheet.append(markSheet[i][0])
+        
     nameSheet.sort()
-    for _ in range(len(nameSheet))
-        print(nameSheet[_])
+    for i in range(len(nameSheet)):
+        print(nameSheet[i])
