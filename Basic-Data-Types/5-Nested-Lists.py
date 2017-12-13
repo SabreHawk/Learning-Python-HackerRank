becf7390113378fb59b62123b7e9f75ea96c01f2
Zhiquan.Wang <mySabreHawk@outlook.com>
2017-12-13 21:37:02 +0800

    Add Solution: Basic-Data-Types-6 2017/12/13 - 21:37

--- Basic-Data-Types/5-Nested-Lists.py
+++ Basic-Data-Types/5-Nested-Lists.py
@@ -1,20 +1,27 @@
-import operator
-if __name__ == '__main__':
-    markSheet = []
-    nameSheet = []
-    for _ in range(int(input())):
-        name = input()
-        score = float(input())
-        markSheet.append([name,score])
-    markSheet.sort(key = operator.itemgetter(1))
-    max_score = markSheet[0][1]
-    sec_score = -1;
-    for i in range(len(markSheet)):
-        if markSheet[i][1] != max_score and sec_score == -1:
-            sec_score = markSheet[i][1]
-        if sec_score != -1 and markSheet[i][1] == sec_score:
-            nameSheet.append(markSheet[i][0])
+solution = 1
+if solution == 1:
+    a = [[input(), float(input())] for i in range(int(input()))]
+    s = sorted(set([x[1] for x in a]))
+    for name in sorted(x[0] for x in a if x[1] == s[1]):
+        print (name)
+elif solution == 2:
+    import operator
+    if __name__ == '__main__':
+        markSheet = []
+        nameSheet = []
+        for _ in range(int(input())):
+            name = input()
+            score = float(input())
+            markSheet.append([name,score])
+        markSheet.sort(key = operator.itemgetter(1))
+        max_score = markSheet[0][1]
+        sec_score = -1;
+        for i in range(len(markSheet)):
+            if markSheet[i][1] != max_score and sec_score == -1:
+                sec_score = markSheet[i][1]
+            if sec_score != -1 and markSheet[i][1] == sec_score:
+                nameSheet.append(markSheet[i][0])
         
-    nameSheet.sort()
-    for i in range(len(nameSheet)):
-        print(nameSheet[i])
+        nameSheet.sort()
+        for i in range(len(nameSheet)):
+            print(nameSheet[i])
