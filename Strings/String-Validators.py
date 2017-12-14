0f5640962f53f2c8a028d2b3259eddfda5c5083d
Zhiquan.Wang <mySabreHawk@outlook.com>
2017-12-14 16:16:25 +0800

    Update Strings-SV.py 2017/12/14 - 16:17

--- Strings/String-Validators.py
+++ Strings/String-Validators.py
@@ -1,21 +1,30 @@
-if __name__ == '__main__':
-    str = input()
-    detecArr = [0 for i in range(5)]
-    for s in str:
-        if s.isalnum():
-            detecArr[0] = 1
-        if s.isalpha():
-            detecArr[1] = 1
-        if s.isdigit():
-            detecArr[2] = 1
-        if s.islower():
-            detecArr[3] = 1
-        if s.isupper():
-            detecArr[4] = 1
-    for i in range(len(detecArr)):
-        if detecArr[i] == 1:
-            print("True")
-        else :
-            print("False")
-        
-   
+solution = 1 
+if solution == 1:
+    S = input()
+    print (any([char.isalnum() for char in S]))
+    print (any([char.isalpha() for char in S]))
+    print (any([char.isdigit() for char in S]))
+    print (any([char.islower() for char in S]))
+    print (any([char.isupper() for char in S]))
+else:
+    if __name__ == '__main__':
+        str = input()
+        detecArr = [0 for i in range(5)]
+        for s in str:
+            if s.isalnum():
+                detecArr[0] = 1
+            if s.isalpha():
+                detecArr[1] = 1
+            if s.isdigit():
+                detecArr[2] = 1
+            if s.islower():
+                detecArr[3] = 1
+            if s.isupper():
+                detecArr[4] = 1
+        for i in range(len(detecArr)):
+            if detecArr[i] == 1:
+                print("True")
+            else :
+                print("False")
+            
+    
