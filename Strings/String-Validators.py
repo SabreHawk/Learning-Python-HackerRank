faacf161e2c61b365bfb51e649dc2b24c0562987
Zhiquan.Wang <mySabreHawk@outlook.com>
2017-12-14 16:13:19 +0800

    Add Solution Strings-SV.py 2017/12/14 - 16:14

--- Strings/String-Validators.py
+++ Strings/String-Validators.py
@@ -1,2 +1,21 @@
-a = "1234"
-print(a[1:1])
+if __name__ == '__main__':
+    str = input()
+    detecArr = [0 for i in range(5)]
+    for s in str:
+        if s.isalnum():
+            detecArr[0] = 1
+        if s.isalpha():
+            detecArr[1] = 1
+        if s.isdigit():
+            detecArr[2] = 1
+        if s.islower():
+            detecArr[3] = 1
+        if s.isupper():
+            detecArr[4] = 1
+    for i in range(len(detecArr)):
+        if detecArr[i] == 1:
+            print("True")
+        else :
+            print("False")
+        
+   
